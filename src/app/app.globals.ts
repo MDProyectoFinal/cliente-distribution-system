bbfea6b4e84df3df9a56ee879c31353aed9e0422
Diego Barberis <diegobarberis20@gmail.com>
2024-07-25 17:49:59 -0300

    Maquetado login y registro

---
+++ src/app/app.globals.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class Globals{
+
+    logoImage = './assets/img/logo-rosario-snack.png';
+    logoImageAlt = 'rosario-snack'
+
+}
+   
