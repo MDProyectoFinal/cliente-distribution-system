a43c0f790279128dc547205e7b2fcec453fee1a5
Leonel Villa <leovilla91@gmail.com>
2024-08-22 20:01:32 -0300

    Estilo editar producto

--- src/app/productos/components/editar-producto/editar-producto.component.ts
+++ src/app/productos/components/editar-producto/editar-producto.component.ts
@@ -17,12 +17,10 @@ export class EditarProductoComponent implements OnInit {
   urlImagen: string | ArrayBuffer | null;
   imagenSubir: any;
 
-
   constructor(private route: ActivatedRoute, private servicio: ProductoService) {}
   ngOnInit(): void {
-
-    this.id = this.route.snapshot.params['id']
-    this.isAgregar = !this.id
+    this.id = this.route.snapshot.params['id'];
+    this.isAgregar = !this.id;
     this.servicio.recuperarTiposProductos().subscribe({
       next: (data) => {
         this.tiposProductos = data;
@@ -35,47 +33,54 @@ export class EditarProductoComponent implements OnInit {
       this.servicio.recuperarProducto(this.id).subscribe({
         next: (data) => {
           this.producto = data;
-          this.urlImagen = data.imagen
+          this.urlImagen = data.imagen;
         },
 
         error: (e) => {},
       });
-
     }
   }
 
-  onImageChanged(eventoCambio :any){
-     const archivos = eventoCambio.target.files
-     if(archivos.length === 0){
-      return
-     }
+  onImageChanged(eventoCambio: any) {
+    const archivos = eventoCambio.target.files;
+    if (archivos.length === 0) {
+      return;
+    }
 
-     const tipoImagen = archivos[0].type
-     if(tipoImagen.match(/image\/*/) == null) {
-      alert("Solamente se aceptan imágenes")
-     }
+    const tipoImagen = archivos[0].type;
+    if (tipoImagen.match(/image\/*/) == null) {
+      alert('Solamente se aceptan imágenes');
+    }
 
-     this.imagenSubir = archivos[0]
-     const reader = new FileReader();
-     reader.readAsDataURL(archivos[0])
-     reader.onload = (_event) =>{
-      this.urlImagen = reader.result
-     }
-
+    this.imagenSubir = archivos[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(archivos[0]);
+    reader.onload = (_event) => {
+      this.urlImagen = reader.result;
+    };
   }
 
-  enviar(){
-    if(this.isAgregar){
-      this.servicio.insertarImagen(this.producto, this.imagenSubir).subscribe({
-      next: (data) => {
-        console.log("exito");
+  enviar() {
+    if (this.isAgregar) {
+      this.servicio.insertarProducto(this.producto, this.imagenSubir).subscribe({
+        next: (data) => {
+          console.log('exito');
+        },
 
-      },
+        error: (e) => {
+          console.log(e);
+        },
+      });
+    } else {
+      this.servicio.editarProducto(this.producto, this.imagenSubir).subscribe({
+        next: (data) => {
+          console.log('exito');
+        },
 
-      error: (e) => {console.log(e)},
-    });
-    } else{
-
+        error: (e) => {
+          console.log(e);
+        },
+      });
     }
   }
 }
