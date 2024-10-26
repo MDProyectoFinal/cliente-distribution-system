8b6f0afbb882049b05e3828e8e8fe41d6de0f57b
Leonel Villa <leovilla91@gmail.com>
2024-10-26 17:22:21 -0300

    Cargar cantidad en carrito para mostrar en nav desde local storage

--- src/app/nav/components/principal-nav/principal-nav.component.ts
+++ src/app/nav/components/principal-nav/principal-nav.component.ts
@@ -1,27 +1,27 @@
+import { LocalStorageService } from './../../../shared/services/local-storage.service';
 import { CarritoPedidoService } from './../../../productos/services/carrito-pedido.service';
 import { identity, Subscription } from 'rxjs';
 import { AuthenticationService } from '../../../usuarios/services/authentication.service';
 import { Component } from '@angular/core';
-import { faBars, faBell, faCartShopping, faGear } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faBell, faCartShopping, faGear } from '@fortawesome/free-solid-svg-icons';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'nav-principal',
   templateUrl: './principal-nav.component.html',
-  styleUrls: ['./principal-nav.component.scss']
+  styleUrls: ['./principal-nav.component.scss'],
 })
 export class PrincipalNavComponent {
-  login : boolean = false;
+  login: boolean = false;
   jwtHelper = new JwtHelperService();
   public decodedToken: any;
   public imagen: string = '';
   public nombreUsuario: string = '';
-  cantidadCarritoSub: Subscription
+  cantidadCarritoSub: Subscription;
   cantidadEnCarrito: number;
 
-  constructor(private authService: AuthenticationService, private  carritoService:CarritoPedidoService ){
-
-    this.login = this.authService.estaAutenticado()
+  constructor(private authService: AuthenticationService, private carritoService: CarritoPedidoService) {
+    this.login = this.authService.estaAutenticado();
 
     var token = localStorage.getItem('token') as string;
     this.decodedToken = this.jwtHelper.decodeToken(token);
@@ -29,12 +29,17 @@ export class PrincipalNavComponent {
     this.imagen = this.authService.usuarioActual?.imagen ?? this.authService.decodedToken.imagen;
     this.nombreUsuario = this.authService.usuarioActual?.nombre_usuario ?? this.authService.decodedToken.nombre_usuario;
 
-    this.cantidadCarritoSub = carritoService.cantidadEnCarrito$.subscribe(
-      cantidad => {this.cantidadEnCarrito = cantidad}
-    )
+    this.cantidadCarritoSub = carritoService.cantidadEnCarrito$.subscribe((cantidad) => {
+      this.cantidadEnCarrito = cantidad;
+    });
 
-    carritoService.setCantidadEnCarrito(0)
+    this.cargarCantidadEnCarrito();
+  }
 
+  private cargarCantidadEnCarrito() {
+    let cantidadLocal = localStorage.getItem('cantidad_carrito');
+    let cantidad = cantidadLocal ? parseInt(cantidadLocal) : 0;
+    this.carritoService.setCantidadEnCarrito(cantidad);
   }
 
   public cerrarSesion(): void {
@@ -43,7 +48,6 @@ export class PrincipalNavComponent {
 
   ngOnDestroy(): void {
     this.cantidadCarritoSub.unsubscribe();
-
   }
 
   menuHamburguesaIcon = faBars;
