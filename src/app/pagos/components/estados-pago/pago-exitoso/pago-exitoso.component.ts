9981c00012b05bad98553cf9444335b230588574
Leonel Villa <leovilla91@gmail.com>
2025-02-28 07:30:02 -0300

    Completar pago pendiente de pedido

--- src/app/pagos/components/estados-pago/pago-exitoso/pago-exitoso.component.ts
+++ src/app/pagos/components/estados-pago/pago-exitoso/pago-exitoso.component.ts
@@ -1,5 +1,6 @@
 import { AfterContentInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PagoService } from 'src/app/pagos/services/pago.service';
 import { PedidoService } from 'src/app/pedidos/services/pedido.service';
 import { CarritoPedidoService } from 'src/app/productos/services/carrito-pedido.service';
 
@@ -9,18 +10,35 @@ import { CarritoPedidoService } from 'src/app/productos/services/carrito-pedido.
   styleUrls: ['./pago-exitoso.component.scss'],
 })
 export class PagoExitosoComponent implements AfterContentInit, OnInit {
-  constructor(private router: Router, private carritoService: CarritoPedidoService, private pedidoService: PedidoService) {}
+  params :URLSearchParams
+  constructor(private router: Router, private carritoService: CarritoPedidoService, private pagoService: PagoService) {}
   ngOnInit(): void {
-    const params = new URLSearchParams(window.location.search);
-    let status = params.get('collection_status');
+    this.params = new URLSearchParams(window.location.search);
+
+
+    let status = this.params.get('collection_status');
     if (status != 'approved') {
       this.volverInicio();
     }
   }
 
   ngAfterContentInit(): void {
-    if (this.carritoService.getProductos().length >= 1) {
-      this.pedidoService.guardarPedido().subscribe({
+
+    const pid = this.params.get('pid');
+    console.log(pid);
+
+    if(pid){
+
+      this.pagoService.registrarPago(pid).subscribe({
+        error: (e) => {
+          console.log(e);
+        },
+      });
+
+    }
+
+    else if (this.carritoService.getProductos().length >= 1) {
+      this.pagoService.guardarPedido().subscribe({
         next: (data) => {
           this.carritoService.limpiar();
         },
--- src/app/pagos/services/pago.service.ts
+++ src/app/pagos/services/pago.service.ts
@@ -1,31 +1,64 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { GLOBAL } from 'src/app/config/global';
+import { CarritoPedidoService } from 'src/app/productos/services/carrito-pedido.service';
+import { AuthenticationService } from 'src/app/usuarios/services/authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PagoService {
 
-  public url: string;
-  private access_token: string = 'APP_USR-3595488656828811-112515-31c2962634f5c18f330f4bb8ead67d88-2116426284' // Del VENDEDOR (TEST)
 
-  constructor( private _http: HttpClient ){
+  registrarPago(pid: string) {
+
+    return this._http.post<{}>(this.url + `pagos/pedido/${pid}/pagar`,{}).pipe(
+      map((response: any) => {
+        if (response.length == 0) return { mensaje: 'No se pudo realizar el pago' };
+        return response;
+      })
+    );
+  }
+
+  pagarPedido(idPedido: string) {
+
+    const data = {
+      payer_email : this.authenticationService.decodedToken.email
+    }
+
+    return this._http.post<{}>(this.url + `pagos/pedido/${idPedido}`, data).pipe(
+      map((response: any) => {
+        if (response.length == 0) return { mensaje: 'No se pudo realizar el pago' };
+
+        // TODO 1: En esta etapa podemos poner ya el estado del pedido como "pendiente"
+
+        // TODO 2: VER CUAL USAR!! Para mi misma pestaña total vuelve con el back url
+        // Redirigir al usuario al checkout de Mercado Pago
+        //window.open(response.data.init_point, '_blank'); // PESTAÑA NUEVA
+        window.location.href = response.data.init_point; // MISMA PESTAÑA
+
+        return response;
+      })
+    );
+
+  }
+  public url: string;
+  private access_token: string = 'APP_USR-3595488656828811-112515-31c2962634f5c18f330f4bb8ead67d88-2116426284'; // Del VENDEDOR (TEST)
+
+  constructor(private _http: HttpClient, private carritoService: CarritoPedidoService, private authenticationService: AuthenticationService) {
     this.url = GLOBAL.url;
   }
 
   createPreference(data: any) {
     let headers = new HttpHeaders({
-      'Content-Type':'application/json',
-      Authorization: `Bearer ${ this.access_token }`
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.access_token}`,
     });
 
-    return this._http.post<{}>(this.url + 'pagos/', data, { headers: headers })
-    .pipe(
+    return this._http.post<{}>(this.url + 'pagos/', data, { headers: headers }).pipe(
       map((response: any) => {
-
-        if( response.length == 0 ) return { mensaje:'No se pudo realizar el pago'};
+        if (response.length == 0) return { mensaje: 'No se pudo realizar el pago' };
 
         // TODO 1: En esta etapa podemos poner ya el estado del pedido como "pendiente"
 
@@ -39,4 +72,25 @@ export class PagoService {
     );
   }
 
+  guardarPedido(): Observable<any> {
+    const idUsuario = this.authenticationService.decodedToken.sub;
+    const productosInsertar = this.carritoService.getProductos().map((p) => {
+      return { idProducto: p.producto._id, cantidad: p.cantidad };
+    });
+
+    const body = {
+      idUsuario: idUsuario,
+      productos: productosInsertar,
+    };
+
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this._http.post(this.url + 'pagos/pedido', body, { headers: headers }).pipe(
+      map((response: any) => {
+        return response;
+      })
+    );
+  }
 }
