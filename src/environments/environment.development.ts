ffe910dfce9a6214538a720d2659eae1cfd2351c
Leonel Villa <leovilla91@gmail.com>
2025-08-12 06:28:44 -0300

    Botón de descarga de manuales

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    captchaSiteKey: '6Lcmn2UrAAAAAN6dljGe2ErPZdUg9rL66oEpUBCg'
+    captchaSiteKey: '6Lcmn2UrAAAAAN6dljGe2ErPZdUg9rL66oEpUBCg',
+    apiURL:'http://localhost:3977/api/'
 };
